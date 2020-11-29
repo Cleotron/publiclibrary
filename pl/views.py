3d406c7c83dda0158b61c8e3dfcb946f06768fef
ana <hidalgo.ana@gmail.com>
2020-11-29 21:40:00 +0100

    formatting

--- pl/views.py
+++ pl/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 
+
 def book_list(request):
     books = Book.objects.all().order_by("title")
-    return render(request, 'pl/book_list.html', {'books': books})
+    return render(request, "pl/book_list.html", {"books": books})
