ca7ab394ed9f417accf2e6bc8259594ed58dad97
Cleotron <Cleotron@users.noreply.github.com>
2020-12-01 20:59:17 +0100

    make a visible page with list of books (#4)
    * make a visible page with list of books
    
    * formatting
    
    * make tests
    
    * make tests

--- pl/tests.py
+++ pl/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+from django.test import TestCase
+from pl.models import Book
+
+
+class BookTestCase(TestCase):
+    def setUp(self):
+        Book.objects.create(author="pepe", title="libro1", in_use=False, reservations=0)
+        Book.objects.create(author="pipo", title="libro2", in_use=True, reservations=2)
+        Book.objects.create(author="pupu", title="libro3", in_use=True, reservations=0)
+
+    def test_included_books(self):
+        response = self.client.get("/")
+        self.assertContains(response, "libro1", html=True)
+        self.assertContains(response, "libro2", html=True)
+        self.assertContains(response, "libro3", html=True)
--- pl/views.py
+++ pl/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+
+def book_list(request):
+    books = Book.objects.all().order_by("title")
+    return render(request, "pl/book_list.html", {"books": books})
